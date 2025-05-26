6f5037e73e996c6fe5ac3f51f3c56f80bc06776d
MasadRayan <masadrayan2002@gmail.com>
2025-05-26 12:49:12 +0600

    Deploy done

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const coffeeCollection = client.db('coffeeDB').collection('coffees');
     const userCollection = client.db('coffeeDB').collection('users');
@@ -104,7 +104,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
